0b46681fbeb3bd5b336a7b1300c1236bfd98d37a
fbdthree <112531356+fbdthree@users.noreply.github.com>
2022-12-19 12:27:22 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const PORT = 8080
+const api = require("./routes/apiRoutes")
+const html = require("./routes/htmlRoutes")
+
+
+app.use(express.urlencoded({extended:true})) 
+app.use(express.json())
+app.use(express.static("public")) // front end files
+
+
+app.use(api)
+app.use(html)
+
+
+
+app.listen(PORT, function(){
+    console.log(`App running on port: http://localhost"${PORT}`)
+})
